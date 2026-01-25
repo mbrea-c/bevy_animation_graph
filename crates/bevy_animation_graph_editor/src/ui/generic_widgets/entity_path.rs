796d019613920f428cf39bbb9ed17ea0aa440c16
Manuel Brea <m.brea.carreras@gmail.com>
2026-01-25 14:10:36 +0000

    fix entitypath widget

--- crates/bevy_animation_graph_editor/src/ui/generic_widgets/entity_path.rs
+++ crates/bevy_animation_graph_editor/src/ui/generic_widgets/entity_path.rs
@@ -17,15 +17,43 @@ impl<'a> EntityPathWidget<'a> {
 impl<'a> egui::Widget for EntityPathWidget<'a> {
     fn ui(self, ui: &mut egui::Ui) -> egui::Response {
         ui.push_id(self.id_hash, |ui| {
-            let mut data = self.entity_path.to_slashed_string();
+            let buffer_id = ui.id().with("entity path slashed string buffer");
 
-            let response = ui.text_edit_singleline(&mut data);
+            // clear buffer if outdated
+            ui.memory_mut(|mem| {
+                let prev = mem.data.get_temp::<Buffer>(buffer_id);
+                if let Some(prev) = prev
+                    && &prev.original != self.entity_path
+                {
+                    mem.data.remove_temp::<Buffer>(buffer_id);
+                }
+            });
+
+            let mut buffer = ui.memory_mut(|mem| {
+                mem.data
+                    .get_temp_mut_or_insert_with(buffer_id, || Buffer {
+                        value: self.entity_path.to_slashed_string(),
+                        original: self.entity_path.clone(),
+                    })
+                    .clone()
+            });
+
+            let response = ui.text_edit_singleline(&mut buffer.value);
+
             if response.changed() {
-                *self.entity_path = EntityPath::from_slashed_string(data);
+                *self.entity_path = EntityPath::from_slashed_string(buffer.value.clone());
             }
+
+            ui.memory_mut(|mem| mem.data.insert_temp(buffer_id, buffer));
 
             response
         })
         .inner
     }
+}
+
+#[derive(Clone, Default)]
+struct Buffer {
+    value: String,
+    original: EntityPath,
 }
