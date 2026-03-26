fa82aba09a93cdfabdbdeecb4f25f039a71db4ea
fogi <77100166+jkukatzki@users.noreply.github.com>
2026-03-26 22:51:27 +0100

    make use of bone mask weights in blend pose node (#131)
    Currently the blend node does not use the bone mask weights to blend but
    rather always uses the factor provided to the node and just checks if
    the weight is positive. IMO it feels more intuitive to also use the bone
    weights in the calculation which is what the commit in this branch
    changes.
    
    Co-authored-by: jkukatzki <jkukatzki@gmail.com>

--- crates/bevy_animation_graph_core/src/interpolation/linear.rs
+++ crates/bevy_animation_graph_core/src/interpolation/linear.rs
@@ -23,15 +23,17 @@ pub struct LinearInterpolator {
 impl LinearInterpolator {
     pub fn interpolate_pose(&self, base: &mut Pose, overlay: &Pose, f: f32) {
         for (bone_id, bone_index) in overlay.paths.iter() {
-            if self.bone_mask.bone_weight(bone_id) == 0. {
+            let bone_weight = self.bone_mask.bone_weight(bone_id);
+            if bone_weight == 0. {
                 continue;
             }
 
+            let scaled_f = f * bone_weight;
             let overlay_bone_pose = &overlay.bones[*bone_index];
 
             if let Some(base_index) = base.paths.get(bone_id) {
                 let base_bone_pose = &mut base.bones[*base_index];
-                base_bone_pose.linear_blend_mut(overlay_bone_pose, f);
+                base_bone_pose.linear_blend_mut(overlay_bone_pose, scaled_f);
             } else {
                 base.add_bone(overlay_bone_pose.clone(), *bone_id);
             }
